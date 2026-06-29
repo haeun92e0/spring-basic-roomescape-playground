684129b8418f083044f558a335e72ad375bc40a0
haeun92e0 <gracekim8729@gmail.com>
2026-06-29 17:57:45 +0900

    Feat: WebMvcConfigurer 설정을 통해 커스텀 아규먼트 리솔버 추가 등록

---
+++ src/main/java/roomescape/WebMvcConfiguration.java
@@ -0,0 +1,16 @@
+package roomescape;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import roomescape.member.LoginMemberArgumentResolver;
+
+import java.util.List;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
+}
