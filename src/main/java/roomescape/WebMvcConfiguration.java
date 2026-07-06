69c254fb44c894581019de7446dd557d854f17f0
haeun92e0 <gracekim8729@gmail.com>
2026-07-06 16:38:32 +0900

    Refactor: AdminInterceptor 등록

--- src/main/java/roomescape/WebMvcConfiguration.java
+++ src/main/java/roomescape/WebMvcConfiguration.java
@@ -3,7 +3,9 @@ package roomescape;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import roomescape.member.AdminInterceptor;
 import roomescape.member.LoginMemberArgumentResolver;
 
 import java.util.List;
@@ -19,6 +21,12 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginMemberArgumentResolver(secretKey));
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(new AdminInterceptor(secretKey))
+                .addPathPatterns("/admin/**", "/admin");
     }
 }
 //LoginMemberArgumentResolver을 스프링 MVC에 ㄷ등록해서 사용할 수 있게 해줌
