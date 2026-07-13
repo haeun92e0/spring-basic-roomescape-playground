bced3cbefdd567f88281054b9fb38006d2d2b96d
haeun92e0 <gracekim8729@gmail.com>
2026-07-13 14:20:43 +0900

    Refactor: 로그인 회원 리졸버가 MemberRepository를 참조하도록 수정

--- src/main/java/roomescape/member/LoginMemberArgumentResolver.java
+++ src/main/java/roomescape/member/LoginMemberArgumentResolver.java
@@ -14,9 +14,11 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 //컨트롤러의 매개변수를 자동으로 만들어주는 클래스
 public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
     private final JwtTokenProvider jwtTokenProvider;
+    private final MemberRepository memberRepository;
 
-    public LoginMemberArgumentResolver(JwtTokenProvider jwtTokenProvider){
+    public LoginMemberArgumentResolver(JwtTokenProvider jwtTokenProvider, MemberRepository memberRepository){
         this.jwtTokenProvider = jwtTokenProvider;
+        this.memberRepository = memberRepository;
     }
 
     @Override
@@ -45,10 +47,8 @@ public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolve
         Claims claims = jwtTokenProvider.parseToken(token);
         //정보 꺼내기
         Long id = Long.parseLong(claims.getSubject());
-        String name = claims.get("name", String.class);
-        String role = claims.get("role", String.class);
 
-        return new Member(id, name, "", role); // 파싱된 정보로 임시 Member 객체 생성하여 반환
+        return memberRepository.findById(id); // 파싱된 정보로 임시 Member 객체 생성하여 반환
     }
 }
 //브라우저가 보낸 JWT 쿠키를 읽어서 그 안에 저장된 회원 정보를 Member 객체로 복원한 뒤, 컨트롤러의 Member loginMember 매개변수에 자동으로
