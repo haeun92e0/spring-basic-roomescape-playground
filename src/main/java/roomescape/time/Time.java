edeca4d5542025cf4bbd3b063102dbb3696505bb
haeun92e0 <gracekim8729@gmail.com>
2026-07-13 14:21:57 +0900

    feat: Time 클래스를 JPA 엔티티로 매핑

--- src/main/java/roomescape/time/Time.java
+++ src/main/java/roomescape/time/Time.java
@@ -1,8 +1,21 @@
 package roomescape.time;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "time")
 public class Time {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "time_value", nullable = false)
     private String value;
+
+    @Column(nullable = false)
+    private boolean deleted = false;
+
 
     public Time(Long id, String value) {
         this.id = id;
