9d73b08b4f59efee7d0832b04f6b84102b3bc154
haeun92e0 <gracekim8729@gmail.com>
2026-07-13 14:22:30 +0900

    feat: Reservation 클래스를 JPA 엔티티로 매핑 및 Member 연관관계 추가

--- src/main/java/roomescape/reservation/Reservation.java
+++ src/main/java/roomescape/reservation/Reservation.java
@@ -1,28 +1,50 @@
 package roomescape.reservation;
 
+import jakarta.persistence.*;
+import roomescape.member.Member;
 import roomescape.theme.Theme;
 import roomescape.time.Time;
 
+
+@Entity
+@Table(name = "reservation")
 public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String date;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "time_id")
     private Time time;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "theme_id")
     private Theme theme;
 
-    public Reservation(Long id, String name, String date, Time time, Theme theme) {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    public Reservation(Long id, String name, String date, Time time, Theme theme, Member member) {
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
         this.theme = theme;
+        this.member = member;
     }
 
-    public Reservation(String name, String date, Time time, Theme theme) {
+    //id 없는 버전
+    public Reservation(String name, String date, Time time, Theme theme, Member member) {
         this.name = name;
         this.date = date;
         this.time = time;
         this.theme = theme;
+        this.member = member;
     }
 
     public Reservation() {
@@ -47,5 +69,9 @@ public class Reservation {
 
     public Theme getTheme() {
         return theme;
+    }
+
+    public Member getMember(){
+        return member;
     }
 }
